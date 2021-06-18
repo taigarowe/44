d22f1968287d7dcf909f1d7a161ec3cafd23884b
taigarowe <70992313+taigarowe@users.noreply.github.com>
2021-06-18 16:23:06 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,10 @@
+var PLAY=1;
+var END=0;
+var gameState=PLAY
 var oscar;
-var ground
+var ground, invisibleGround;
+var spike, lava, powerUp, poisonousPlant;
+var spikeGroup, lavaGroup, poisonousPlantGroup, powerUpGroup;
 
 
 function preload() {
@@ -11,18 +16,53 @@ function setup() {
   oscar = createSprite(200,450,50,50);
   oscar.shapeColor = "red"
 
-  ground = createSprite(675,500, 1350, 20);
+  ground = createSprite(675,500, 2700, 20);
+  ground.x = ground.width/2;
+
+  invisibleGround = createSprite(675,500,1250,10);
+  invisibleGround.visible = false;
+
+  spikeGroup = new Group();
+  poisonousPlantGroup = new Group();
+  powerUpGroup = new Group();
+  lavaGroup = new Group();
 }
 
 function draw() {
 background("black");
 
-if(keyWentDown("UP_ARROW")) {
-  oscar.velocityY = - 20;
+if(gameState===PLAY){
+  ground.velocityX = -5;
+  if (ground.x < 0) {
+    ground.x=ground.width/2
+  }
+  
+  if(keyDown("UP_ARROW")&& player.y >= 100) {
+    player.velocityY = -20;
+  }
+  
+  oscar.velocityY = oscar.velocityY + 1;
 }
 
-oscar.velocityY = oscar.velocityY + 1;
 
-oscar.collide(ground);
+
 drawSprites();
+}
+
+function spike(){
+ if(frameCount)
+  spike = createSprite();
+
+}
+
+function lava(){
+
+}
+
+function powerUp(){
+
+}
+
+function poisonousPlant(){
+
 }
